48c8abf97955072d3ab9df37c1a2bb15c9cd30fb
danbox <danbox19@msn.com>
2014-02-26 16:00:20 -0700

    Added TCPReceiverThread and TCPServerThread

--- cs455/scaling/transport/TCPSender.java
+++ cs455/scaling/transport/TCPSender.java
@@ -3,10 +3,25 @@ package cs455.scaling.transport;
 /**
  * @author Dan Boxler
  */
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class TCPSender
 {
-    public TCPSender()
-    {
+    private DataOutputStream 	_dout;
 
+    public TCPSender(Socket socket) throws IOException
+    {
+        _dout = new DataOutputStream(socket.getOutputStream());
+    }
+
+    public void sendData(byte[] dataToSend) throws IOException
+    {
+        int dataLength = dataToSend.length;
+        _dout.writeInt(dataLength);
+        _dout.write(dataToSend, 0, dataLength);
+        _dout.flush();
     }
 }
